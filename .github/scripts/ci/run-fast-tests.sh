fcf44a088bad40d5f410ebaa3c43d1ffa26d2c81
Robin <pipeyy@rs-web.net>
2026-04-29 17:30:05 +0200

    feat: increase JVM memory allocation for fast and full test runs
    Signed-off-by: Robin <pipeyy@rs-web.net>

--- .github/scripts/ci/run-fast-tests.sh
+++ .github/scripts/ci/run-fast-tests.sh
@@ -9,4 +9,4 @@ set -euo pipefail
 echo "SOCT_CHISEL_VERSION=${SOCT_CHISEL_VERSION:-<unset>}"
 echo "SOCT_TEST_RUN_DIR=${SOCT_TEST_RUN_DIR:-<unset>}"
 
-sbt 'testOnly soct.tests.SimulationSpec -- -t "Fast test should run without errors"'
+sbt -J-Xmx8G 'testOnly soct.tests.SimulationSpec -- -t "Fast test should run without errors"'
