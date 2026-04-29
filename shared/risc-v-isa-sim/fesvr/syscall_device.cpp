39817341608b21368ad69d220fa30182a478c342
Robin <pipeyy@rs-web.net>
2026-04-29 13:24:47 +0200

    feat: enhance syscall_device with flexible syscall registration and Windows compatibility
    Signed-off-by: Robin <pipeyy@rs-web.net>

--- shared/risc-v-isa-sim/fesvr/syscall_device.cpp
+++ shared/risc-v-isa-sim/fesvr/syscall_device.cpp
@@ -5,6 +5,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#ifdef _WIN32
+#include <io.h>
+#define read _read
+#define write _write
+#define close _close
+#define lseek _lseek
+#define fstat _fstat64
+#define stat __stat64
+#endif
+
 #include "syscall_device.hpp"
 #include "soct/syscalls.h"
 #include "soct/soct_ff.h"
@@ -109,91 +119,179 @@ namespace {
         s.st_nlink = 1;
         s.st_blksize = 64;
     }
+
+    int64_t sysret_errno(int64_t ret) {
+        return ret == -1 ? -errno : ret;
+    }
+
+    uint64_t syscall_any(const uint32_t sysno, const uint64_t a0, const uint64_t a1, const uint64_t a2,
+                         const uint64_t a3, const uint64_t a4, const uint64_t a5, const uint64_t a6) {
+        soct::logging::fesvr::debug << "Unknown syscall " << sysno << " with args " << a0 << " " << a1 << " " << a2 << " "
+                << a3 << " " << a4 << " " << a5 << " " << a6 << "\n";
+#ifdef _WIN32
+        return -ENOSYS;
+#else
+        return sysret_errno(syscall(sysno, a0, a1, a2, a3, a4, a5, a6));
+#endif
+    }
 } // namespace
 
-size_t fds_t::alloc(const sreg_t fd) {
-    uint64_t i;
-    for (i = 0; i < m_fds.size(); i++)
-        if (m_fds[i] == -1) // a deallocated fd is available
-            break;
-
-    if (i == m_fds.size())
-        m_fds.resize(i + 1);
-
-    m_fds[i] = fd;
-    return i;
-}
-
-void fds_t::dealloc(const sreg_t fd) {
-    if (fd >= 0 && static_cast<size_t>(fd) < m_fds.size())
-        m_fds[fd] = -1;
-}
-
-std::pair<sreg_t, fds_t::fd_type> fds_t::lookup(const size_t fd) const {
-    if (static_cast<int>(fd) == RV_AT_FDCWD)
-        return {RV_AT_FDCWD, any_t};
-
-    // check if requested fd is i/o fd
-    if (fd >= m_fds.size()) {
-        return {-1, invalid_t};
-    }
-    if (m_fds[fd] == -1) {
-        return {-1, invalid_t};
-    }
-    // Here we compare against the integers and not macros as they come from the target and are specific to the software running on it
-    // We just have to assume that these requests are valid
-    if (fd == 0) {
-        return {m_fds[0], stdin_t};
-    }
-    if (fd == 1) {
-        return {m_fds[1], stdout_t};
-    }
-    if (fd == 2) {
-        return {m_fds[2], stderr_t};
-    }
-
-    return {m_fds[fd], any_t};
-}
-
-syscall_device_t::~syscall_device_t() {
-    // close all system file descriptors
-    for (const auto i: m_fds_system) {
-        close(m_fds.lookup(i).first);
-        m_fds.dealloc(i);
-    }
-}
+syscall_device_t::~syscall_device_t() = default;
 
 syscall_device_t::syscall_device_t(const std::shared_ptr<htif_t> &htif, const std::shared_ptr<chunked_memif_t> &cmemif)
     : device_t(htif, cmemif) {
     using namespace std::literals;
-    // Posix syscalls:
-    m_table[SOCT_READ] = [this](auto... args) { return sys_read(args...); };
-    m_table[SOCT_WRITE] = [this](auto... args) { return sys_write(args...); };
-    m_table[SOCT_OPEN] = [this](auto... args) { return sys_open(args...); };
-    m_table[SOCT_CLOSE] = [this](auto... args) { return sys_close(args...); };
-    m_table[SOCT_FSTAT] = [this](auto... args) { return sys_fstat(args...); };
-    m_table[SOCT_LSEEK] = [this](auto... args) { return sys_lseek(args...); };
-    m_table[SOCT_EXIT] = [this](auto... args) { return sys_exit(args...); };
-    m_table[SOCT_OPENAT] = [this](auto... args) { return sys_openat(args...); };
 
-    // Custom Syscalls (implement C functions)
-    m_table[SOCT_PATHCONF] = [this](auto... args) { return sys_pathconf(args...); };
+    register_syscall(SOCT_READ, [this](uint64_t fd, uint64_t pbuf, uint64_t len) -> uint64_t {
+        soct::logging::fesvr::debug << "Reading " << len << " bytes from fd " << fd << " to " << pbuf << "\n";
+        std::vector<char> buf(len);
+        const ssize_t ret = sysret_errno(read(fd, buf.data(), len));
+        if (ret >= 0)
+            m_cmemif->write(pbuf, ret, reinterpret_cast<uint8_t *>(buf.data()));
+        return ret;
+    });
+
+    register_syscall(SOCT_WRITE, [this](uint64_t fd, uint64_t pbuf, uint64_t len) -> uint64_t {
+        soct::logging::fesvr::debug << "Writing " << len << " bytes to fd " << fd << " from " << pbuf << "\n";
+        std::vector<char> buf(len);
+        m_cmemif->read(pbuf, len, reinterpret_cast<uint8_t *>(buf.data()));
+
+        if (fd == 1) {
+            soct::logging::elf::to_stdout << std::string(buf.data(), len);
+            return len;
+        }
+        if (fd == 2) {
+            soct::logging::elf::to_stderr << std::string(buf.data(), len);
+            return len;
+        }
+        return sysret_errno(write(fd, buf.data(), len));
+    });
+
+    register_syscall(SOCT_FSTAT, [this](uint64_t fd, uint64_t pbuf) -> uint64_t {
+        soct::logging::fesvr::debug << "fstat on fd " << fd << " to " << pbuf << "\n";
+
+        soct_stat s{};
+
+        int ret;
+        if (fd == 0 || fd == 1 || fd == 2) {
+            make_guest_stdio_stat(s);
+            ret = 0;
+        } else {
+            struct stat st{};
+            ret = sysret_errno(fstat(fd, &st));
+            if (ret == 0)
+                s = to_soct_stat(st);
+        }
+
+        if (ret == 0) {
+            soct::logging::fesvr::debug << "fstat result: st_size=" << s.st_size
+                    << " st_mode=0" << std::oct << s.st_mode << std::dec
+                    << " st_blocks=" << s.st_blocks
+                    << " st_atime=" << s.st_atime_sec << "\n";
+            m_cmemif->write(pbuf, sizeof(s), reinterpret_cast<const uint8_t *>(&s));
+        }
+        return ret;
+    });
+
+    register_syscall(SOCT_OPEN, [this](uint64_t pname, uint64_t flags, uint64_t mode) -> uint64_t {
+        auto host_mode = mode;
+        auto host_flags = static_cast<uint64_t>(translate_open_flags(flags));
+
+        const auto name = m_cmemif->read_string(pname, MAX_PATH_SIZE);
+        soct::logging::fesvr::debug << "Opening file " << name << " with flags " << host_flags << " and mode "
+                << host_mode << "\n";
+#ifdef _WIN32
+        return sysret_errno(_open(name.c_str(), host_flags, host_mode));
+#else
+        return sysret_errno(open(name.c_str(), host_flags, host_mode));
+#endif
+    });
+
+    register_syscall(SOCT_CLOSE, [](uint64_t fd) -> uint64_t {
+        soct::logging::fesvr::debug << "Closing fd " << fd << "\n";
+        if (fd == 0 || fd == 1 || fd == 2)
+            return 0;
+        return sysret_errno(close(fd));
+    });
+
+    register_syscall(SOCT_LSEEK, [](uint64_t fd, uint64_t ptr, uint64_t whence) -> uint64_t {
+        soct::logging::fesvr::debug << "Seeking fd " << fd << " to " << ptr << " with whence " << whence << "\n";
+        return sysret_errno(lseek(fd, ptr, whence));
+    });
+
+    register_syscall(SOCT_EXIT, [this](uint64_t code) -> uint64_t {
+        soct::logging::fesvr::info << "Elf is exiting with code " << code << "\n";
+        m_htif->set_exitcode(static_cast<int32_t>(code));
+        return 0;
+    });
+
+    register_syscall(SOCT_OPENAT, [this](uint64_t dirfd, uint64_t pname, uint64_t flags, uint64_t mode) -> uint64_t {
+        int host_dirfd = (int)dirfd;
+        const auto name = m_cmemif->read_string(pname, MAX_PATH_SIZE);
+        const auto host_flags = translate_open_flags(flags);
+        soct::logging::fesvr::debug << "Opening file " << name << " with flags " << flags << " and mode " << mode
+                << " in directory " << dirfd << "\n";
+
+#ifdef _WIN32
+        if (host_dirfd == RV_AT_FDCWD || host_dirfd == -100) {
+            return sysret_errno(_open(name.c_str(), host_flags, mode));
+        }
+        return -ENOSYS;
+#else
+        if (host_dirfd == RV_AT_FDCWD)
+            host_dirfd = AT_FDCWD;
+
+        return sysret_errno(openat(host_dirfd, name.c_str(), host_flags, mode));
+#endif
+    });
+
+    register_syscall(SOCT_PATHCONF, [this](uint64_t path, uint64_t param) -> uint64_t {
+        soct::logging::fesvr::debug << "Getting pathconf for " << path << " with param " << param << "\n";
+#ifdef _WIN32
+        // Windows does not implement pathconf natively.
+        // Param 3 = _PC_NAME_MAX, 4 = _PC_PATH_MAX according to standard POSIX layout.
+        switch (param) {
+            case 3: return 255;
+            case 4: return 260; // MAX_PATH
+            default: return -ENOSYS;
+        }
+#else
+        const auto host_path = m_cmemif->read_string(path, MAX_PATH_SIZE);
+        return sysret_errno(pathconf(host_path.c_str(), param));
+#endif
+    });
+
     // TODO implement SOCT_FOPEN
-    m_table[SOCT_GET_MAINVARS] = [this](auto... args) { return sys_getmainvars(args...); };
-    m_table[SOCT_HTIF_DEV_TEST] = [](auto...) { return 0; };
+    register_syscall(SOCT_GET_MAINVARS, [this](uint64_t pbuf, uint64_t limit) -> uint64_t {
+        soct::logging::fesvr::debug << "Getting main vars" << "\n";
+        const auto &args = m_htif->target_args();
+        using chunk_t = uint64_t; // TODO Why 64 bit?
+        std::vector<chunk_t> words(args.size() + 3);
+        words[0] = args.size();
+        words[args.size() + 1] = 0; // argv[argc] = NULL
+        words[args.size() + 2] = 0; // envp[0] = NULL
+
+        size_t sz = words.size() * sizeof(chunk_t);
+        for (size_t i = 0; i < args.size(); i++) {
+            words[i + 1] = pbuf + sz;
+            sz += args[i].length() + 1;
+        }
+
+        std::vector<char> bytes(sz);
+        std::memcpy(bytes.data(), words.data(), sizeof(chunk_t) * words.size());
+        for (size_t i = 0; i < args.size(); i++)
+            std::strcpy(&bytes[words[i + 1] - pbuf], args[i].c_str());
+
+        if (bytes.size() > limit)
+            return -ENOMEM;
+
+        m_cmemif->write(pbuf, bytes.size(), reinterpret_cast<const uint8_t *>(bytes.data()));
+        return 0;
+    });
+
+    register_syscall(SOCT_HTIF_DEV_TEST, []() -> uint64_t { return 0; });
 
     register_command(0, [this](const cmd_t &command) { handle_syscall(command); }, "syscall"sv);
-
-    const int stdin_fd = dup(STDIN_FILENO);
-    const int stdout_fd = dup(STDOUT_FILENO);
-    const int stderr_fd = dup(STDERR_FILENO);
-    if (stdin_fd < 0 || stdout_fd < 0 || stderr_fd < 0)
-        throw std::runtime_error("could not dup stdin/stdout");
-
-    // fds[0] = stdin, fds[1] = stdout, fds[2] = stderr (due to order of allocation)
-    m_fds_system.push_back(m_fds.alloc(stdin_fd));
-    m_fds_system.push_back(m_fds.alloc(stdout_fd));
-    m_fds_system.push_back(m_fds.alloc(stderr_fd));
 }
 
 void syscall_device_t::handle_syscall(const cmd_t &cmd) {
@@ -222,189 +320,4 @@ void syscall_device_t::handle_syscall(const cmd_t &cmd) {
     m_cmemif->write(mm, sizeof(htif_mem), reinterpret_cast<uint8_t *>(htif_mem.data()));
     soct::logging::fesvr::debug << "Syscall " << syscall << " handled\n";
     cmd.respond(1);
-}
-
-int64_t syscall_device_t::sysret_errno(int64_t ret) {
-    return ret == -1 ? -errno : ret;
-}
-
-uint64_t syscall_device_t::sys_read(const uint64_t fd, const uint64_t pbuf, const uint64_t len, uint64_t, uint64_t,
-                                    uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Reading " << len << " bytes from fd " << fd << " to " << pbuf << "\n";
-    std::vector<char> buf(len);
-    const ssize_t ret = sysret_errno(read(m_fds.lookup(fd).first, buf.data(), len));
-    if (ret >= 0)
-        m_cmemif->write(pbuf, ret, reinterpret_cast<uint8_t *>(buf.data()));
-    return ret;
-}
-
-uint64_t syscall_device_t::sys_write(const uint64_t fd, const uint64_t pbuf, const uint64_t len, uint64_t, uint64_t,
-                                     uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Writing " << len << " bytes to fd " << fd << " from " << pbuf << "\n";
-    std::vector<char> buf(len);
-    m_cmemif->read(pbuf, len, reinterpret_cast<uint8_t *>(buf.data()));
-
-    // here we have to check where the fd is pointing to, so we can direct it to the correct stream
-    const auto [fd_, type] = m_fds.lookup(fd);
-    if (type == fds_t::stdout_t) {
-        soct::logging::elf::to_stdout << std::string(buf.data(), len);
-        // Return number of bytes written
-        return len;
-    }
-    if (type == fds_t::stderr_t) {
-        soct::logging::elf::to_stderr << std::string(buf.data(), len);
-        // Return number of bytes written
-        return len;
-    }
-    // Otherwise, write to the file descriptor
-    return sysret_errno(write(fd_, buf.data(), len));
-}
-
-uint64_t syscall_device_t::sys_fstat(const uint64_t fd, const uint64_t pbuf, uint64_t, uint64_t, uint64_t, uint64_t,
-                                     uint64_t) {
-    soct::logging::fesvr::debug << "fstat on fd " << fd << " to " << pbuf << "\n";
-
-    const auto [host_fd, type] = m_fds.lookup(fd);
-    if (type == fds_t::invalid_t)
-        return -EBADF;
-
-    soct_stat s{};
-
-    int ret;
-    if (type == fds_t::stdin_t || type == fds_t::stdout_t || type == fds_t::stderr_t) {
-        make_guest_stdio_stat(s);
-        ret = 0;
-    } else {
-        struct stat st{};
-        ret = sysret_errno(fstat(host_fd, &st));
-        if (ret == 0)
-            s = to_soct_stat(st);
-    }
-
-    if (ret == 0) {
-        soct::logging::fesvr::debug << "fstat result: st_size=" << s.st_size
-                << " st_mode=0" << std::oct << s.st_mode << std::dec
-                << " st_blocks=" << s.st_blocks
-                << " st_atime=" << s.st_atime_sec << "\n";
-        m_cmemif->write(pbuf, sizeof(s), reinterpret_cast<const uint8_t *>(&s));
-    }
-    return ret;
-}
-
-uint64_t syscall_device_t::sys_open(const uint64_t pname, const uint64_t flags, const uint64_t mode, uint64_t, uint64_t,
-                                    uint64_t, uint64_t) {
-    auto host_mode = mode;
-    auto host_flags = static_cast<uint64_t>(translate_open_flags(flags));
-
-#ifdef _WIN32
-    throw std::runtime_error("open not implemented on Windows");
-#else
-    const auto name = m_cmemif->read_string(pname, MAX_PATH_SIZE);
-    soct::logging::fesvr::debug << "Opening file " << name << " with flags " << host_flags << " and mode "
-            << host_mode << "\n";
-    const int64_t fd = sysret_errno(open(name.c_str(), host_flags, host_mode));
-    if (fd < 0)
-        return fd;
-    return m_fds.alloc(fd);
-#endif
-}
-
-uint64_t syscall_device_t::sys_close(const uint64_t fd, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Closing fd " << fd << "\n";
-    const auto [fd_, type] = m_fds.lookup(fd);
-    if (type == fds_t::invalid_t)
-        return -EBADF;
-    if (type == fds_t::any_t || type == fds_t::stdin_t || type == fds_t::stdout_t || type == fds_t::stderr_t) {
-        const auto ret = sysret_errno(close(fd_));
-        if (ret < 0)
-            return ret;
-        m_fds.dealloc(fd);
-    }
-    return 0;
-}
-
-uint64_t syscall_device_t::sys_lseek(const uint64_t fd, const uint64_t ptr, const uint64_t whence, uint64_t, uint64_t,
-                                     uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Seeking fd " << fd << " to " << ptr << " with whence " << whence << "\n";
-    return sysret_errno(lseek(m_fds.lookup(fd).first, ptr, whence));
-}
-
-uint64_t syscall_device_t::sys_exit(const uint64_t code, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t) {
-    soct::logging::fesvr::info << "Elf is exiting with code " << code << "\n";
-    m_htif->set_exitcode(static_cast<int32_t>(code));
-    return 0;
-}
-
-uint64_t syscall_device_t::sys_openat(const uint64_t dirfd, const uint64_t pname, const uint64_t flags,
-                                      const uint64_t mode, uint64_t, uint64_t, uint64_t) {
-#ifdef _WIN32
-    throw std::runtime_error("openat not implemented on Windows");
-#else
-    const auto [host_dirfd, type] = m_fds.lookup(dirfd);
-    if (type == fds_t::invalid_t)
-        return -EBADF;
-
-    const auto name = m_cmemif->read_string(pname, MAX_PATH_SIZE);
-    const auto host_flags = translate_open_flags(flags);
-    soct::logging::fesvr::debug << "Opening file " << name << " with flags " << flags << " and mode " << mode
-            << " in directory " << dirfd << "\n";
-    const sreg_t fd = sysret_errno(openat(host_dirfd, name.c_str(), host_flags, mode));
-    if (fd < 0) {
-        return fd;
-    }
-    return m_fds.alloc(fd);
-#endif
-}
-
-uint64_t syscall_device_t::sys_pathconf(const uint64_t path, const uint64_t param, uint64_t, uint64_t, uint64_t,
-                                        uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Getting pathconf for " << path << " with param " << param << "\n";
-#ifdef _WIN32
-    throw std::runtime_error("pathconf not implemented on Windows");
-#else
-    const auto host_path = m_cmemif->read_string(path, MAX_PATH_SIZE);
-    return sysret_errno(pathconf(host_path.c_str(), param));
-#endif
-}
-
-
-uint64_t syscall_device_t::sys_getmainvars(const uint64_t pbuf, const uint64_t limit, uint64_t, uint64_t, uint64_t,
-                                           uint64_t, uint64_t) {
-    soct::logging::fesvr::debug << "Getting main vars" << "\n";
-    const auto &args = m_htif->target_args();
-    using chunk_t = uint64_t; // TODO Why 64 bit?
-    std::vector<chunk_t> words(args.size() + 3);
-    words[0] = args.size();
-    words[args.size() + 1] = 0; // argv[argc] = NULL
-    words[args.size() + 2] = 0; // envp[0] = NULL
-
-    size_t sz = words.size() * sizeof(chunk_t);
-    for (size_t i = 0; i < args.size(); i++) {
-        words[i + 1] = pbuf + sz;
-        sz += args[i].length() + 1;
-    }
-
-    std::vector<char> bytes(sz);
-    std::memcpy(bytes.data(), words.data(), sizeof(chunk_t) * words.size());
-    for (size_t i = 0; i < args.size(); i++)
-        std::strcpy(&bytes[words[i + 1] - pbuf], args[i].c_str());
-
-    if (bytes.size() > limit)
-        return -ENOMEM;
-
-    m_cmemif->write(pbuf, bytes.size(), reinterpret_cast<const uint8_t *>(bytes.data()));
-    return 0;
-}
-
-uint64_t syscall_device_t::syscall_any(const uint32_t sysno, const uint64_t a0, const uint64_t a1, const uint64_t a2,
-                                       const uint64_t a3,
-                                       const uint64_t a4, const uint64_t a5,
-                                       const uint64_t a6) {
-    soct::logging::fesvr::debug << "Unknown syscall " << sysno << " with args " << a0 << " " << a1 << " " << a2 << " "
-            << a3 << " " << a4 << " " << a5 << " " << a6 << "\n";
-#ifdef _WIN32
-    throw std::runtime_error("Unknown syscall " + std::to_string(sysno) + " not available on Windows");
-#else
-    return sysret_errno(syscall(sysno, a0, a1, a2, a3, a4, a5, a6));
-#endif
 }
--- shared/risc-v-isa-sim/fesvr/syscall_device.hpp
+++ shared/risc-v-isa-sim/fesvr/syscall_device.hpp
@@ -6,38 +6,7 @@
 #include <functional>
 #include "device.hpp"
 #include "types.hpp"
-#include "logging.hpp"
 #include "memif.hpp"
-
-#ifndef _WIN32
-#include <fcntl.h>
-#endif
-
-/// File descriptor allocator
-class fds_t {
-public:
-    enum fd_type {
-        invalid_t = -1,
-        stdin_t = 0,
-        stdout_t = 1,
-        stderr_t = 2,
-        any_t = 3
-    };
-
-  	/// Allocate a file descriptor
-    size_t alloc(sreg_t fd);
-
-    /// Deallocate a file descriptor
-    void dealloc(sreg_t fd);
-
-    /// Lookup a file descriptor
-    [[nodiscard]] std::pair<sreg_t, fd_type> lookup(size_t fd) const;
-
-    ~fds_t() = default;
-private:
-  	/// The file descriptors
-    std::vector<uint64_t> m_fds;
-};
 
 class syscall_device_t final : public device_t {
 public:
@@ -58,43 +27,24 @@ private:
     /// The command handler for the syscall device
     void handle_syscall(const cmd_t& cmd);
 
-    /// Helper function to return -errno if the return value is -1
-    static int64_t sysret_errno(int64_t ret);
-
-    uint64_t sys_read(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_write(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_open(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_close(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_fstat(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_lseek(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_exit(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_openat(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    /// Custom syscalls:
-    uint64_t sys_pathconf(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    uint64_t sys_getmainvars(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-    /// Use the index of syscall and pass it to OS:
-    uint64_t syscall_any(uint32_t sysno, uint64_t a0, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, uint64_t a5, uint64_t a6);
-
     /// Maximum number of syscalls
     constexpr static size_t MAX_SYSCALLS = 2048;
 
     /// Syscall table
-    std::vector<syscall_func_t> m_table{MAX_SYSCALLS};
+    using flex_syscall_func_t = std::function<uint64_t(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t)>;
+    std::vector<flex_syscall_func_t> m_table{MAX_SYSCALLS};
 
-    /// File descriptor allocator
-    fds_t m_fds;
-
-    /// Contains the file descriptors from the system
-    std::vector<uint64_t> m_fds_system;
-
+    template <typename F>
+    void register_syscall(size_t index, F&& func) {
+        m_table[index] = [f = std::forward<F>(func)](uint64_t a0, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, uint64_t a5, uint64_t a6) -> uint64_t {
+            if constexpr (std::is_invocable_v<F>) return f();
+            else if constexpr (std::is_invocable_v<F, uint64_t>) return f(a0);
+            else if constexpr (std::is_invocable_v<F, uint64_t, uint64_t>) return f(a0, a1);
+            else if constexpr (std::is_invocable_v<F, uint64_t, uint64_t, uint64_t>) return f(a0, a1, a2);
+            else if constexpr (std::is_invocable_v<F, uint64_t, uint64_t, uint64_t, uint64_t>) return f(a0, a1, a2, a3);
+            else if constexpr (std::is_invocable_v<F, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t>) return f(a0, a1, a2, a3, a4);
+            else if constexpr (std::is_invocable_v<F, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t>) return f(a0, a1, a2, a3, a4, a5);
+            else return f(a0, a1, a2, a3, a4, a5, a6);
+        };
+    }
 };
